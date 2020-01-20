eb0fef63efebed2ecd34978a1dc1e1deaadf6457
Albert Alonso Benitez <albert15_89@hotmail.com>
2020-01-20 22:02:22 +0100

    Feat: vista Home

--- MarianaoWars/ClientApp/src/components/Home.js
+++ MarianaoWars/ClientApp/src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import '../css/marianao_style.css';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <div className="layout">
+                <div></div>
+                <div></div>
+            </div>
+        );
+    }
 }
