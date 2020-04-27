eaa64a22b237a56a9c0a7c8086c23d3508f962b0
Albert Alonso Benitez <albert15_89@hotmail.com>
2020-04-27 08:45:45 +0200

    Feat: systemsoftware

---
+++ MarianaoWars/Models/SystemSoftware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarianaoWars.Models
+{
+    public class SystemSoftware
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int LastVersion { get; set; }
+
+        public string Memmory { get; set; }
+
+        public string Action1 { get; set; }
+
+        public string Time { get; set; }
+
+        public string RequireKnowledge { get; set; }
+
+        public string RequireIngenyous { get; set; }
+
+        public string RequireCoffee { get; set; }
+    }
+}
