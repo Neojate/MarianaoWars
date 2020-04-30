b995c5107b99f6fd5dd1111796939bb902a346a8
Eloy Nadal Escobar <bydoser@gmail.com>
2020-04-30 17:15:20 +0200

    systems

--- MarianaoWars/ClientApp/src/components/Game/NavSystems.js
+++ MarianaoWars/ClientApp/src/components/Game/NavSystems.js
@@ -1,6 +1,8 @@
 ﻿import React, { Component } from 'react';
 import { Row, Col, Container } from 'reactstrap';
 import { System } from './System';
+import systemServices from '../services/SytemServices';
+
 import $ from "jquery";
 
 
@@ -14,13 +16,42 @@ export class NavSystems extends Component {
         //this.sistemActive = this.sistemActive.bind(this, id);
 
         this.state = {
-            
+
+            systemActive: 0,
+            systems: [],
+            loading: true
+
         };
     }
 
     componentDidMount() {
+        this.getSystemResource();
         
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        
+        // Uso tipico (no olvides de comparar los props):
+        if (this.state.systemActive !== prevState.systemActive) {
+            if (this.state.systemActive != 0) {
+                $('.sub-nav').show(500);
+            }
+        }
+    }
+
+    async getSystemResource() {
+        var systemResource = await systemServices.systemResourceData();
+
+        var stateTemp = this.state.systems.slice();
+        stateTemp[1] = systemResource;
+        
+        this.setState({
+            systems: stateTemp,
+            loading: false
+        })        
+    }
+
+
 
     navStyle() {
         return {
@@ -47,35 +78,69 @@ export class NavSystems extends Component {
         }
     }
 
-    sistemActive(id) {
+    async sistemActive(id) {
 
-        console.log(id);
-        $('.sub-nav').show("fast");
+        $('.sub-nav').hide(500);
+
+        if (this.state.systemActive == id) {
+            id = 0;
+            await this.sleep(400);
+        }
+        this.setState({
+            systemActive: id
+        });
+
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+
+    drawSubMenu() {
+
+        return (
+            <Row style={this.subNavStyle()} className={"align-items-center sub-nav"}>
+                {this.state.systems[this.state.systemActive].map((system, index) => {
+                    return <System key={index} />;
+                })}
+            </Row>
+         );
+    }
+
+
+    draw() {
+        return (
+                <Row style={this.navStyle()} className={"d-flex align-items-center"} >
+                    <System name={"resource"} system={this.state.systemResources} onClick={this.sistemActive.bind(this, 1)} />
+                    <System onClick={this.sistemActive.bind(this, 2)} />
+                    <System onClick={this.sistemActive.bind(this, 3)} />
+                    <System onClick={this.sistemActive.bind(this, 4)} />
+                    <System onClick={this.sistemActive.bind(this, 5)} />
+                    <System onClick={this.sistemActive.bind(this, 6)} />
+                </Row>            
+        );
     }
 
 
     render() {
 
+        let nav = this.state.loading
+            ? ''
+            : this.draw();
+
+        let subnav = '';
+
+        //Si tenemos un system activo y este tiene elementos, lo mostramos
+        if (this.state.systemActive != 0 && this.state.systems[this.state.systemActive] != undefined) {
+            subnav = this.drawSubMenu();
+        }
+
+        
         return (
-
             <>
-            <Row style={this.subNavStyle()} className={"align-items-center sub-nav"}>
-                <System />
-                <System />
-                <System />
-                <System />
-                <System />
-                <System />
-            </Row>
-
-                <Row style={this.navStyle()} className={"d-flex align-items-center"} >
-                    <System onClick={this.sistemActive.bind(this, 1)}/>
-                    <System onClick={this.sistemActive.bind(this, 2)}/>
-                    <System onClick={this.sistemActive.bind(this, 3)}/>
-                    <System onClick={this.sistemActive.bind(this, 4)}/>
-                    <System onClick={this.sistemActive.bind(this, 5)}/>
-                    <System onClick={this.sistemActive.bind(this, 6)}/>
-            </Row>
+              {subnav}
+              {nav}
             </>
         );
     }
